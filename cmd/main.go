03604651effb10054664918db12b1385aa4bed17
Nick S <solonin.n.s@gmail.com>
2025-05-05 15:14:47 +0300

    kjhg

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"time"
+
 	"github.com/n1tees/BookingKart-Platform/config"
 	"github.com/n1tees/BookingKart-Platform/internal/db"
 )
 
 func main() {
 	config.LoadEnv()
+
+	time.Sleep(10 * time.Second)
+
 	db.InitDB()
 
 	// Здесь позже запустим сервер
